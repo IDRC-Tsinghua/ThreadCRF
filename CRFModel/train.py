c44536c5e11110ba437edb46cfa1ed9aed9ebb08
dongchao <dongchao@xiaomi.com>
2015-07-15 18:03:04 +0900

    dev

--- CRFModel/train.py
+++ CRFModel/train.py
@@ -72,7 +72,7 @@ if __name__ == '__main__':
 
     crf = EdgeFeatureGraphCRF(n_states=3, n_features=len(node_features) + dictLength,
                               n_edge_features=len(edge_features), class_weight=[1.3, 0.8, 1.0])
-    ssvm = OneSlackSSVM(crf, inference_cache=100, C=.1, tol=.001, max_iter=10000, n_jobs=3)
+    ssvm = OneSlackSSVM(crf, inference_cache=100, C=1, tol=.001, max_iter=10000, n_jobs=3)
 
     accuracy = 0.0
     total_correct = 0
